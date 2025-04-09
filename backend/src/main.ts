00917864243f67b1fec23d505efbe2980f586770
Gaurav <07gaurav04kumar03@gmail.com>
2025-04-09 07:52:35 +0530

    Added TLS and SSl connection to mongodb

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Enable CORS for frontend (Next.js)
+  app.enableCors({
+    origin: 'http://localhost:3000', // frontend origin
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
